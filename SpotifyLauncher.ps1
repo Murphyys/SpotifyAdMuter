830afa731fa41a8c6c07cc3d1d5c0e486e18e780
Ice <129828852+Murphyys@users.noreply.github.com>
2026-06-11 23:01:22 +0700

    fix: use cmd /c start spotify: for Store version (simpler, works in all sessions)

--- SpotifyLauncher.ps1
+++ SpotifyLauncher.ps1
@@ -10,10 +10,7 @@ $classicExe = "$env:APPDATA\Spotify\Spotify.exe"
 if ((Test-Path $classicExe) -and (Get-Item $classicExe).Length -gt 0) {
     Start-Process $classicExe
 } elseif (Test-Path $storeStub) {
-    $pkg = Get-AppxPackage -Name "*Spotify*" -ErrorAction SilentlyContinue | Select-Object -First 1
-    if ($pkg) {
-        Start-Process "explorer.exe" -ArgumentList "shell:AppsFolder\$($pkg.PackageFamilyName)!Spotify"
-    }
+    Start-Process "cmd.exe" -ArgumentList "/c start spotify:" -WindowStyle Hidden
 } else {
     Add-Type -AssemblyName System.Windows.Forms
     [System.Windows.Forms.MessageBox]::Show('Spotify not found on this machine.', 'Spotify AdMuter') | Out-Null
